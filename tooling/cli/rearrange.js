3626f827a4aa1a9f8d1c9ef056afe2beaab2aedc
Vasilis Kefallinos <vkefallinos@gmail.com>
2021-11-01 15:02:17 +0200

    update packages

--- tooling/cli/rearrange.js
+++ tooling/cli/rearrange.js
@@ -3,25 +3,36 @@ const fs = require('fs');
 const path = require('path');
 
 const parserPackages = glob.sync('../../parsers/*/*/package.json');
+const libsPackages = glob.sync('../../libs/*/package.json');
 
+const rush = require('../../rush.json');
+rush.projects.forEach((project) => {
+  const pkgFilePath = path.join('../..', project.projectFolder, 'package.json');
+  const pkg = require(pkgFilePath);
+  pkg.homepage = `https://github.com/gen-codes/astql/tree/main/${project.projectFolder}`;
+  pkg.main = 'index.js';
+  fs.writeFileSync(pkgFilePath, JSON.stringify(pkg, null, 2));
+});
+// libsPackages.forEach((packagePath) => {});
 parserPackages.forEach((packagePath) => {
-  const pkg = require(packagePath);
+  const parserPkg = require(packagePath);
   const parserFolder = path.dirname(packagePath);
   const tsconfig = require(path.join(parserFolder, 'tsconfig.json'));
   tsconfig.compilerOptions.types = ['jest', 'node'];
-  pkg.devDependencies = pkg.devDependencies || {};
-  pkg.devDependencies['@digigov/cli-lint'] = '0.5.25';
-  pkg.devDependencies['eslint'] = '~7.24.0';
-  pkg.devDependencies['prettier'] = '~2.2.1';
-  pkg.devDependencies = {
-    ...pkg.devDependencies,
+  parserPkg.main = 'index.js';
+  parserPkg.devDependencies = parserPkg.devDependencies || {};
+  parserPkg.devDependencies['@digigov/cli-lint'] = '0.5.25';
+  parserPkg.devDependencies['eslint'] = '~7.24.0';
+  parserPkg.devDependencies['prettier'] = '~2.2.1';
+  parserPkg.devDependencies = {
+    ...parserPkg.devDependencies,
     '@types/jest': '27.0.2',
     '@types/node': '~16.11.6',
     '@digigov/cli-lint': '~0.5.25',
   };
-  pkg.peerDependencies['@astql/languages'] = '0.1.3';
-  pkg.scripts.watch = 'digigov build --watch';
-  pkg.scripts.lint = 'digigov lint';
+  parserPkg.peerDependencies['@astql/languages'] = '0.1.3';
+  parserPkg.scripts.watch = 'digigov build --watch';
+  parserPkg.scripts.lint = 'digigov lint';
   const eslintrc = `module.exports = {
   extends: [
     require.resolve('@digigov/cli-lint/eslint.config'),
@@ -37,7 +48,7 @@ parserPackages.forEach((packagePath) => {
   fs.writeFileSync(path.join(parserFolder, '.eslintrc.js'), eslintrc);
   fs.writeFileSync(
     path.join(parserFolder, 'package.json'),
-    JSON.stringify(pkg, null, 2)
+    JSON.stringify(parserPkg, null, 2)
   );
   fs.writeFileSync(
     path.join(parserFolder, 'tsconfig.json'),
---
+++ tooling/cli/src/index.ts
@@ -0,0 +1,3 @@
+export { default as App } from 'astql/App';
+export * from 'astql/Init';
+export * from 'astql/cli';
